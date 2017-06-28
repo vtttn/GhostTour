1c16817180515ed64d61d5ba741bd68a4d22a034
Vicky Tran <tranvickyt@gmail.com>
2017-06-28 10:50:48 -0500

    lightbox

--- Scripts/script.js
+++ Scripts/script.js
@@ -31,7 +31,15 @@ $(function () {
 });
 
 
-
+// Lightbox
+document.getElementById('links').onclick = function (event) {
+event = event || window.event;
+var target = event.target || event.srcElement,
+    link = target.src ? target.parentNode : target,
+    options = {index: link, event: event},
+    links = this.getElementsByTagName('a');
+blueimp.Gallery(links, options);
+};
 
 
 // console.log($('.parsley-required').html());
